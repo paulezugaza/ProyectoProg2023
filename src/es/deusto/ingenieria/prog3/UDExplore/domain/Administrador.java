55a8f23c7d1faef2df0b67878a57bbd153183026
Begoña Rodriguez <bego.r@opendeusto.es>
2023-10-26 16:22:07 +0200

    ad

---
+++ src/es/deusto/ingenieria/prog3/UDExplore/domain/Administrador.java
@@ -0,0 +1,26 @@
+package es.deusto.ingenieria.prog3.UDExplore.domain;
+
+public class Administrador extends Usuario{
+	
+	private Estancia estanciaAdmin;
+
+	public Administrador(String nombreUsuario, String nombre, String apellido, String correoElectronico,
+			String numeroTelefono, Estancia estanciaAdmin) {
+		super(nombreUsuario, nombre, apellido, correoElectronico, numeroTelefono);
+		this.estanciaAdmin = estanciaAdmin;
+	}
+
+	public Estancia getEstanciaAdmin() {
+		return estanciaAdmin;
+	}
+
+	public void setEstanciaAdmin(Estancia estanciaAdmin) {
+		this.estanciaAdmin = estanciaAdmin;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrador: " + super.toString() + "[estanciaAdmin=" + estanciaAdmin + "]";
+	}	
+
+}
