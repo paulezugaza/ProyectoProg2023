cf5e4fc4a9a99c5adfb8e4af20f95f6c9f26058f
oihanech <oihane.carrillo@opendeusto.es>
2023-11-30 11:54:53 +0100

    io

--- src/es/deusto/ingenieria/prog3/UDExplore/io/BaseDeDatos.java
+++ src/es/deusto/ingenieria/prog3/UDExplore/io/BaseDeDatos.java
@@ -7,12 +7,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import es.deusto.ingenieria.prog3.UDExplore.domain.Administrador;
+import es.deusto.ingenieria.prog3.UDExplore.domain.CadenaHotelera;
 import es.deusto.ingenieria.prog3.UDExplore.domain.Cliente;
+import es.deusto.ingenieria.prog3.UDExplore.domain.Estancia;
+import es.deusto.ingenieria.prog3.UDExplore.domain.Habitacion;
+import es.deusto.ingenieria.prog3.UDExplore.domain.Hotel;
 import es.deusto.ingenieria.prog3.UDExplore.domain.Usuario;
 
 
@@ -22,33 +30,42 @@ public class BaseDeDatos {
 	private static Connection conexion;
 	private static Logger logger = Logger.getLogger( "BaseDeDatos" );
 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	private static HashMap<String,Usuario> users = new HashMap<String, Usuario>();
+	public static HashMap<String,Usuario> users = new HashMap<String, Usuario>();
 	
 	public static boolean abrirConexion( String nombreBD, boolean reiniciaBD ) {
 		try {
-			logger.log( Level.INFO, "Carga de librer铆a org.sqlite.JDBC" );
+			logger.log( Level.INFO, "Carga de libreria org.sqlite.JDBC" );
 			Class.forName("org.sqlite.JDBC");  // Carga la clase de BD para sqlite
-			logger.log( Level.INFO, "Abriendo conexi贸n con " + nombreBD );
+			logger.log( Level.INFO, "Abriendo conexion con " + nombreBD );
 			conexion = DriverManager.getConnection("jdbc:sqlite:" + nombreBD );
 			if (reiniciaBD) {
 				Statement statement = conexion.createStatement();
 				
-				String sent = "CREATE TABLE IF NOT EXISTS usuario(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre varchar(10), email varchar(25), contrasenya varchar(25), admin int);";
+				String sent = "CREATE TABLE IF NOT EXISTS CadenaHotelera(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre varchar(10));";
 				logger.log( Level.INFO, "Statement: " + sent );
 				statement.executeUpdate( sent );
 				
-				sent = "CREATE TABLE IF NOT EXISTS reserva(numeroReserva INTEGER PRIMARY KEY AUTOINCREMENT, fechaInicio bigint, fechaFin bigint, idUsuario INTEGER REFERENCES usuario (id));";
+				sent = "CREATE TABLE IF NOT EXISTS Hotel(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre varchar(10), ciudad varchar(10), foto varchar(20), categoria integer, idCadenaHotelera INTEGER, FOREIGN KEY(idCadenaHotelera) REFERENCES CadenaHotelera(id));";
 				logger.log( Level.INFO, "Statement: " + sent );
 				statement.executeUpdate( sent );
 				
-				sent = "CREATE TABLE IF NOT EXISTS Habitacion(numeroHabitacion INTEGER PRIMARY KEY AUTOINCREMENT, capacidadMax integer, precioPorNoche integer, iDhotel INTEGER REFERENCES hotel(id));";
+				sent = "CREATE TABLE IF NOT EXISTS Habitacion(id INTEGER PRIMARY KEY AUTOINCREMENT, numHabitacion integer, capacidad integer, precioPorNoche integer, idHotel INTEGER, FOREIGN KEY(idHotel) REFERENCES Hotel(id));";
 				logger.log( Level.INFO, "Statement: " + sent );
 				statement.executeUpdate( sent );
-
-				sent = "CREATE TABLE IF NOT EXISTS Hotel(id INTEGER PRIMARY KEY AUTOINCREMENT, cadenaHotelera varchar(10));";
+				
+				sent = "CREATE TABLE IF NOT EXISTS Apartamento(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre varchar(10), ciudad varchar(10), numHabitacion integer, precioPorNoche integer);";
 				logger.log( Level.INFO, "Statement: " + sent );
 				statement.executeUpdate( sent );
-				sent = "CREATE TABLE IF NOT EXISTS ReservasHotel(id INTEGER PRIMARY KEY AUTOINCREMENT, iDHabitacion INTEGER REFERENCES Habitacion(id),iDhotel INTEGER REFERENCES hotel(id));";
+								
+				sent = "CREATE TABLE IF NOT EXISTS Cliente(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre varchar(10), apellido varchar(10), email varchar(25), contrasenya varchar(25));";
+				logger.log( Level.INFO, "Statement: " + sent );
+				statement.executeUpdate( sent );
+				
+				sent = "CREATE TABLE IF NOT EXISTS Reserva(numeroReserva INTEGER PRIMARY KEY AUTOINCREMENT, fechaInicio bigint, fechaFin bigint, idCliente INTEGER, idApartamento INTEGER, idHabitacion INTEGER, FOREIGN KEY(idCliente) REFERENCES Cliente(id), FOREIGN KEY(idApartamento) REFERENCES Apartamento(id), FOREIGN KEY(idHabitacion) REFERENCES Habitacion(id));";
+				logger.log( Level.INFO, "Statement: " + sent );
+				statement.executeUpdate( sent );
+				
+				sent = "CREATE TABLE IF NOT EXISTS Administrador(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre varchar(10), apellido varchar(10), email varchar(25), contrasenya varchar(25), salario real);";
 				logger.log( Level.INFO, "Statement: " + sent );
 				statement.executeUpdate( sent );
 				
@@ -56,81 +73,149 @@ public class BaseDeDatos {
 			
 			return true;
 		} catch(Exception e) {
-			logger.log( Level.SEVERE, "Excepci贸n", e );
+			logger.log( Level.SEVERE, "Excepcion", e );
 			return false;
 		}
 	}	
 	
 	public static void cerrarConexion() {
 		try {
-			logger.log( Level.INFO, "Cerrando conexi贸n" );
+			logger.log( Level.INFO, "Cerrando conexion" );
 			conexion.close();
 		} catch (SQLException e) {
-			logger.log( Level.SEVERE, "Excepci贸n", e );
+			logger.log( Level.SEVERE, "Excepcion", e );
 		}
 	}
 	
-	public static HashMap<String,Usuario> getUsuarios(){
+	public static void cargarUsuarios() {
 		try (Statement statement = conexion.createStatement()){
-			String sent = "select * from usuario;";
+			String sent = "select * from Cliente;";
 			logger.log( Level.INFO, "Statement: " + sent );
 			ResultSet rs = statement.executeQuery( sent );
-			while( rs.next() ) { // Leer el resultset
+			while( rs.next() ) { 
 				int id = rs.getInt("id");
 				String nombre = rs.getString("nombre");
+				String apellido = rs.getString("apellido");
 				String email = rs.getString("email");
 				String constrasenya = rs.getString("contrasenya");
-				if(rs.getInt("admin")==1) {
-					Administrador a =new Administrador(nombre, email, constrasenya );
-					a.setCodigoUsuario(id);
-					users.put(email,  a);		
-				}
-				else {
-					Cliente c = new Cliente(nombre, email, constrasenya, rs.getInt("admin")) ;
-					c.setCodigoUsuario(id);
-					users.put(email,c);
-				}
+				Cliente c = new Cliente(id, nombre, apellido, email, constrasenya) ;
+				users.put(email,c);
 			}
-			return users;
+			String sent1 = "select * from Administrador;";
+			logger.log( Level.INFO, "Statement: " + sent1 );
+			ResultSet rs1 = statement.executeQuery( sent1 );
+			while( rs.next() ) { // Leer el resultset
+				int id = rs.getInt("id");
+				String nombre = rs1.getString("nombre");
+				String apellido = rs1.getString("apellido");
+				String email = rs1.getString("email");
+				String constrasenya = rs1.getString("contrasenya");
+				float salario = rs1.getFloat("salario");
+				Administrador a = new Administrador(id, nombre, apellido, email, constrasenya, salario) ;
+				users.put(email,a);
+			}
 		 } catch (SQLException e) {
-		        logger.log(Level.SEVERE, "Excepci贸n SQL", e);
-		        return null; 
+		        logger.log(Level.SEVERE, "Excepcion SQL", e);
 		    }
 	}
 	
 	
 	
-	public static void a帽adirUsuario(String nombre, String email, String contrasenya) {
+	
+	public static int anyadirCliente(Cliente cliente) {
 		String sent="";
 		try(Statement statement = conexion.createStatement()) {
-			sent = "insert into usuario (nombre, email, contrasenya, admin) values ('" + nombre +"', '" + email + "', '" + contrasenya + "', 0);";
-			logger.log( Level.INFO, "Lanzada actualizaci贸n a base de datos: " + sent );
+			sent = "insert into Cliente (nombre, apellido, email, contrasenya) values ('" + cliente.getNombreUsuario() + "', '" + cliente.getApellido() + "', '" + cliente.getCorreoElectronico() + "', '" + cliente.getContrasenya() + "');";
+			logger.log( Level.INFO, "Lanzada actualizacion a base de datos: " + sent );
 			int val = statement.executeUpdate( sent );
-			logger.log( Level.INFO, "A帽adida " + val + " fila a base de datos\t" + sent );
-			
-		} catch (SQLException e) {
-			logger.log( Level.SEVERE, "Error en inserci贸n de base de datos\t" + e );
-		}
-	}
-	public static int a帽adirReserva( java.util.Date fechaIni, java.util.Date fechaFin, int usuarioId) {
-		String sent="";
-		try(Statement statement = conexion.createStatement()) {
-			sent = "insert reserva (fechaIni, fechaFin, usuarioId ) values (" + fechaIni + ",'" + fechaFin + "', " + usuarioId + ");";
-			logger.log( Level.INFO, "Lanzada actualizaci贸n a base de datos: " + sent );
-			int val = statement.executeUpdate( sent );
-			logger.log( Level.INFO, "A帽adida " + val + " fila a base de datos\t" + sent );
-			sent = "select id from reserva order by id desc;";
+			logger.log( Level.INFO, "A馻dida " + val + " fila a base de datos\t" + sent );
+			sent = "select max(id) from Cliente;";
 			ResultSet rs = statement.executeQuery(sent);
-			while(rs.next()) {
+			if(rs.next()) {
 				int id = rs.getInt("id");
 				return id;
 			}
 		} catch (SQLException e) {
-			logger.log( Level.SEVERE, "Error en inserci贸n de base de datos\t" + e );
+			logger.log( Level.SEVERE, "Error en insercion de base de datos\t" + e );
+		}
+		return 0;
+	}
+	
+	public static int anyadirReserva( java.util.Date fechaIni, java.util.Date fechaFin, int usuarioId) {
+		String sent="";
+		try(Statement statement = conexion.createStatement()) {
+			sent = "insert reserva (fechaIni, fechaFin, usuarioId ) values (" + fechaIni + ",'" + fechaFin + "', " + usuarioId + ");";
+			logger.log( Level.INFO, "Lanzada actualizacion a base de datos: " + sent );
+			int val = statement.executeUpdate( sent );
+			logger.log( Level.INFO, "A馻dida " + val + " fila a base de datos\t" + sent );
+			sent = "select max(numeroReserva) from Reserva;";
+			ResultSet rs = statement.executeQuery(sent);
+			if(rs.next()) {
+				int id = rs.getInt("numeroReserva");
+				return id;
+			}
+		} catch (SQLException e) {
+			logger.log( Level.SEVERE, "Error en insercion de base de datos\t" + e );
 		}
 		return 0;
 	}
 
+	public static Set<String> getCiudades(){
+		Set<String> ciudades = new HashSet<>();
+		try (Statement statement = conexion.createStatement()){
+			String sent = "select distinct ciudad from Hotel;";
+			logger.log( Level.INFO, "Statement: " + sent );
+			ResultSet rs = statement.executeQuery( sent );
+			while( rs.next() ) { 
+				ciudades.add(rs.getString("ciudad"));
+			}
+			String sent1 = "select distinct ciudad from Apartamento;";
+			logger.log( Level.INFO, "Statement: " + sent1 );
+			ResultSet rs1 = statement.executeQuery( sent1 );
+			while( rs1.next() ) { 
+				ciudades.add(rs1.getString("ciudad"));
+			}
+			return ciudades;
+		 } catch (SQLException e) {
+		        logger.log(Level.SEVERE, "Excepcion SQL", e);
+		        return null; 
+		    }
+	}
 	
+	public static List<Estancia> buscarEstancia(String ciudad, long fechaIni, long fechaFin){
+		List<Estancia> estancias = new ArrayList<>();
+		try (Statement statement = conexion.createStatement()){
+			String sent = "SELECT Hotel.*, CadenaHotelera.nombre nombreC FROM Hotel, CadenaHotelera WHERE Hotel.idCadenaHotelera = CadenaHotelera.id AND ciudad = '" + ciudad +"' AND Hotel.id IN (SELECT idHotel FROM Habitacion WHERE id NOT IN(SELECT idHabitacion FROM Reserva WHERE fechaInicio >= '" + fechaIni + "' AND fechaFin <= '" + fechaFin + "'));";
+			logger.log( Level.INFO, "Statement: " + sent );
+			ResultSet rs = statement.executeQuery( sent );
+			while( rs.next() ) { 
+				CadenaHotelera cd = new CadenaHotelera(rs.getInt("idCadenaHotelera"), rs.getString("nombreC"));
+				Hotel hotel = new Hotel(rs.getInt("id"), rs.getString("nombre"), rs.getString("ciudad"), rs.getString("foto"), rs.getInt("categoria"));
+				hotel.setCadenaHotelera(cd);
+				estancias.add(hotel);
+			}
+			return estancias;
+		 } catch (SQLException e) {
+		        logger.log(Level.SEVERE, "Excepcion SQL", e);
+		        return null; 
+		    }
+	}
+	
+	public static List<Habitacion> getHabitaciones(int id){
+		List<Habitacion> habitaciones = new ArrayList<>();
+		try (Statement statement = conexion.createStatement()){
+			String sent = "select * from Habitacion where idHotel = '" + id + "';";
+			logger.log( Level.INFO, "Statement: " + sent );
+			ResultSet rs = statement.executeQuery( sent );
+			while( rs.next() ) { 
+				Habitacion h = new Habitacion(rs.getInt("id"),rs.getInt("numHabitacion"), rs.getInt("capacidad"), rs.getDouble("precioPorNoche"));
+				habitaciones.add(h);
+			}
+			return habitaciones;
+		 } catch (SQLException e) {
+		        logger.log(Level.SEVERE, "Excepcion SQL", e);
+		        return null; 
+		    }
+	}
 	
 }
--- src/es/deusto/ingenieria/prog3/UDExplore/io/Logica.java
+++ src/es/deusto/ingenieria/prog3/UDExplore/io/Logica.java
@@ -1,58 +1,49 @@
 package es.deusto.ingenieria.prog3.UDExplore.io;
 
 
-	import java.io.BufferedReader;
-	import java.io.BufferedWriter;
 	import java.io.FileInputStream;
 	import java.io.FileOutputStream;
-	import java.io.FileReader;
-	import java.io.FileWriter;
 	import java.io.IOException;
 	import java.io.ObjectInputStream;
 	import java.io.ObjectOutputStream;
 	import java.io.Serializable;
-	import java.text.ParseException;
-	import java.text.SimpleDateFormat;
-	import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 	import java.util.Date;
-	import java.util.HashMap;
 	import java.util.List;
-	import java.util.Map;
 	import java.util.logging.Level;
 	import java.util.logging.Logger;
-	import javax.swing.JComboBox;
 
-	import es.deusto.ingenieria.prog3.UDExplore.domain.Apartamento;
-	import es.deusto.ingenieria.prog3.UDExplore.domain.Cliente;
+import javax.swing.JComboBox;
+
+import es.deusto.ingenieria.prog3.UDExplore.domain.Cliente;
 	import es.deusto.ingenieria.prog3.UDExplore.domain.Estancia;
-	import es.deusto.ingenieria.prog3.UDExplore.domain.Hotel;
-	import es.deusto.ingenieria.prog3.UDExplore.domain.Reserva;
+import es.deusto.ingenieria.prog3.UDExplore.domain.Habitacion;
+import es.deusto.ingenieria.prog3.UDExplore.domain.Reserva;
 	import es.deusto.ingenieria.prog3.UDExplore.domain.Usuario;
-
-
 
 	public class Logica implements Serializable{
 		
 		private static final long serialVersionUID = 1L;
-		public static List<Estancia>  estanciasHistoricas = new ArrayList<>();
+//		public static List<Estancia>  estanciasHistoricas = new ArrayList<>();
 		private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		public static Usuario usuario=null;
 		public static Date fechaIni;
 		public static Date fechaFin;
 		
-		public static List<Estancia> getEstanciasHistoricas() {
-			return estanciasHistoricas;
-		}
+//		public static List<Estancia> getEstanciasHistoricas() {
+//			return estanciasHistoricas;
+//		}
 
-		public static boolean UsuarioComprador(String email) {
-			if(BaseDeDatos.getUsuarios().get(email) instanceof Cliente ) return true;
-			else return false;
+		public static boolean esUsuarioCliente(String email) {
+			return BaseDeDatos.users.get(email) instanceof Cliente;
 		}
 		
 
-		public static void setEstanciasHistoricas(List<Estancia> estanciasHistoricas) {
-			Logica.estanciasHistoricas = estanciasHistoricas;
-		}
+//		public static void setEstanciasHistoricas(List<Estancia> estanciasHistoricas) {
+//			Logica.estanciasHistoricas = estanciasHistoricas;
+//		}
 
 		private static Logger logger = Logger.getLogger( "Logica" );
 
@@ -63,138 +54,64 @@ package es.deusto.ingenieria.prog3.UDExplore.io;
 
 
 		
-		public static void guardarEstancias(String nombreFic) {
-			try {
-				ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nombreFic));
-				oos.writeObject(estanciasHistoricas);
-				logger.log( Level.INFO, "Estancias guardadas correctamente en: " + nombreFic );
-				oos.close();
-			}catch(IOException e){
-				logger.log( Level.INFO, "Ha habido un error en la escritura del fichero: " + nombreFic + e);
-			}
-		}
-		
-		
-		public static void cargarEstancias(String nombreFic) {
-			try {
-				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nombreFic));
-				@SuppressWarnings("unchecked")
-				List<Estancia> cCargado = (ArrayList<Estancia>) ois.readObject();
-				estanciasHistoricas=cCargado;
-				ois.close();
-				logger.log( Level.INFO, "Estancias cargadas correctamente desde: " + nombreFic );
-			} catch (IOException | ClassNotFoundException e) {
-				logger.log( Level.INFO, "ERROR EN LA CARGA de fichero: " + nombreFic + e);
-			}
-		}
+//		public static void guardarEstancias(String nombreFic) {
+//			try {
+//				ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nombreFic));
+//				oos.writeObject(estanciasHistoricas);
+//				logger.log( Level.INFO, "Estancias guardadas correctamente en: " + nombreFic );
+//				oos.close();
+//			}catch(IOException e){
+//				logger.log( Level.INFO, "Ha habido un error en la escritura del fichero: " + nombreFic + e);
+//			}
+//		}
+//		
+//		
+//		public static void cargarEstancias(String nombreFic) {
+//			try {
+//				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nombreFic));
+//				@SuppressWarnings("unchecked")
+//				List<Estancia> cCargado = (ArrayList<Estancia>) ois.readObject();
+//				estanciasHistoricas=cCargado;
+//				ois.close();
+//				logger.log( Level.INFO, "Estancias cargadas correctamente desde: " + nombreFic );
+//			} catch (IOException | ClassNotFoundException e) {
+//				logger.log( Level.INFO, "ERROR EN LA CARGA de fichero: " + nombreFic + e);
+//			}
+//		}
 		
 		public static boolean existeUsuario(String email) {
-			if(BaseDeDatos.getUsuarios().containsKey(email)) return true;
-			else return false;
+			return BaseDeDatos.users.containsKey(email);
 		}
-		public static void crearUsuario(String nombre, String email, String contrasenya) {
-			Cliente c1= new Cliente(nombre, email,contrasenya, 0); 
-			BaseDeDatos.getUsuarios().put(c1.getCorreoElectronico(),c1);
+		
+		public static void crearUsuario(String nombre, String apellido, String email, String contrasenya) {
+			Cliente c1 = new Cliente(nombre, apellido, email, contrasenya);
+			int id = BaseDeDatos.anyadirCliente(c1);
+			c1.setCodigoUsuario(id);
+			BaseDeDatos.users.put(c1.getCorreoElectronico(),c1);
 			
 		}
-		public static Usuario usuarioCorrecto(String email, String contrasenya) {
-			if(BaseDeDatos.getUsuarios().get(email).getContraseña().equals(contrasenya)){
-				Logica.usuario=BaseDeDatos.getUsuarios().get(email);
-				logger.log( Level.INFO, "Existe usuario en la BD");
-				return BaseDeDatos.getUsuarios().get(email);
-			}else return null;
-		}
-		
-		private static final String RESERVATIONS_FILE = "resources/data/reservas.csv";
-		private static final String HOTELES_FILE = "resources/data/hoteles.csv";
-		private static final String APARTAMENTOS_FILE = "resources/data/apartamentos.csv";
-		
-		public static void guardarReserva(Reserva reserva) {
-			try (BufferedWriter writer = new BufferedWriter(new FileWriter(RESERVATIONS_FILE, true))) {
-				String line = String.format("%s#%s#%s#%s", 
-										reserva.getCliente().toString(),
-										String.valueOf(reserva.getFechaInicio()),
-										String.valueOf(reserva.getFechaFin()));
-				writer.write(line);
-				writer.newLine();
-	        } catch (Exception ex) {
-	        	logger.warning(String.format("%s - Error guardando reserva: %s", reserva , ex.getMessage()));
-	        }		
-		}
-		
-		public static Map<Cliente, List<Reserva>> cargarMapaReservas() {
-			 Map<Cliente, List<Reserva>> reservasCliente = new HashMap<>();
-			
-			try (BufferedReader reader = new BufferedReader(new FileReader(RESERVATIONS_FILE))) {
-				String line = reader.readLine();
-				Reserva reserva;
-
-				while ((line = reader.readLine()) != null) {					reserva = Reserva.parseCSV(line);
-					reservasCliente.putIfAbsent(reserva.getCliente(), new ArrayList<>());
-					reservasCliente.get(reserva.getCliente()).add(reserva);
+		public static boolean usuarioCorrecto(String email, String contrasenya) {
+			if(BaseDeDatos.users.get(email) != null) {
+				if(BaseDeDatos.users.get(email).getContrasenya().equals(contrasenya)) {
+					usuario = BaseDeDatos.users.get(email);
+					logger.log( Level.INFO, "Usuario correcto");
+					return true;
 				}
-			} catch (Exception ex) {
-				logger.warning(String.format("%s - Error cargando reservas: %s",  ex.getMessage()));
 			}
-			
-			return reservasCliente;
-
+			return false;
 		}
-		
-		public static void guardarHotel(Hotel hotel) {
-			try (BufferedWriter writer = new BufferedWriter(new FileWriter(HOTELES_FILE, true))) {
-				String line = String.format("%s#%s#%s#%s#%s#%s#%s#%s#%s", 
-										hotel.getNombre(),
-										hotel.getCiudad(),
-										hotel.getCategoria(),
-										hotel.getNumeroHabitaciones(),
-										hotel.getTarifaNoche(),
-										hotel.getFoto(),
-										hotel.getReservas(),
-										hotel.getCadenaHotelera(),
-										hotel.getHabitaciones());
-					
-				
-				writer.write(line);
-				writer.newLine();
-	        } catch (Exception ex) {
-	        	logger.warning(String.format("%s - Error guardando reserva: %s", hotel , ex.getMessage()));
-	        }		
-		}
-		
-		
-		public static void guardarApartamento(Apartamento apartamento) {
-			try (BufferedWriter writer = new BufferedWriter(new FileWriter(APARTAMENTOS_FILE, true))) {
-				String line = String.format("%s#%s#%s#%s#%s#%s#%s", 
-										apartamento.getNombre(),
-										apartamento.getCiudad(),
-										apartamento.getCategoria(),
-										apartamento.getNumeroHabitaciones(),
-										apartamento.getTarifaNoche(),
-										apartamento.getFoto(),
-										apartamento.getReservas());
-										
-					
-				
-				writer.write(line);
-				writer.newLine();
-	        } catch (Exception ex) {
-	        	logger.warning(String.format("%s - Error guardando reserva: %s", apartamento, ex.getMessage()));
-	        }		
-		}
-		
-		public static boolean estanciaDisponibleEnFechas(Estancia estancia, Date inicio, Date fin) {
-		    if (estancia.getReservas() == null) {
-		        return true; 
-		    }
-
-		    for (List<Reserva> reserva : estancia.getReservas().values()) {
-		        if (!fin.before(((Reserva) reserva).getFechaInicio()) && !inicio.after(((Reserva) reserva).getFechaFin())) {
-		            return false; 
-		        }
-		    }
-		    return true; 
-		}
+//		public static boolean estanciaDisponibleEnFechas(Estancia estancia, Date inicio, Date fin) {
+//		    if (estancia.getReservas() == null) {
+//		        return true; 
+//		    }
+//
+//		    for (Reserva reserva : habitacion.getReservas()) {
+//		        if (!fin.before(reserva.getFechaInicio()) && !inicio.after(reserva.getFechaFin())) {
+//		            return false; 
+//		        }
+//		    }
+//		    return true; 
+//		}
 
 		public static long parsear(String anyo, String mes, String dia) {
 			String fecha = dia + "/" + mes + "/" + anyo;
